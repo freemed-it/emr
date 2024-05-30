2e54c7222b3fe8a0b8882e33d2afbcd03ac2eb7c
eeseung <eeseungh@gmail.com>
2024-05-31 03:34:58 +0900

    comment: Users entity 컬럼 주석 추가

--- backend/src/users/entity/users.entity.ts
+++ backend/src/users/entity/users.entity.ts
@@ -4,18 +4,22 @@ import { Column, Entity } from 'typeorm';
 
 @Entity()
 export class Users extends BaseModel {
+  /** 사용자 게정 */
   @Column({ length: 20, unique: true })
   account: string;
 
+  /** 사용자 비밀번호 */
   @Column({ length: 100 })
   @Exclude({
     toPlainOnly: true,
   })
   password: string;
 
+  /** 사용자 권한 */
   @Column('char', { length: 4 })
   permission: string;
 
+  /** 사용자 이름 */
   @Column({ length: 20, unique: true })
   name: string;
 }
