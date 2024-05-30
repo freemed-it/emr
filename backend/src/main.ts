a4377e2da1197a9711e8c1ea660b69d4541cd0ed
Seunghee Lee <49032882+eeseung@users.noreply.github.com>
2024-05-30 10:52:55 +0900

    chore(BE): Swagger 설정 (#12)
    * chore: swagger dependency 추가
    
    * feat: swagger 설정
    
    * chore: workflows swagger env 추가

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as expressBasicAuth from 'express-basic-auth';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(
+    ['/api/docs'],
+    expressBasicAuth({
+      challenge: true,
+      users: {
+        [process.env.SWAGGER_USER]: process.env.SWAGGER_PASSWORD,
+      },
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('EMR API')
+    .setDescription('FREEMED EMR (Electronic Medical Record) API')
+    .setVersion('2.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(parseInt(process.env.PORT, 10) || 3000);
 }
 bootstrap();
