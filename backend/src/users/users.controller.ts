753c7f293b6c8e93adbf215a2c7dde077a01c210
eeseung <eeseungh@gmail.com>
2024-05-31 15:48:44 +0900

    feat: 사용자 생성 API 구현

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { CreateUserDto } from './dto/create-user.dto';
 
+@ApiTags('User')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  @ApiOperation({
+    summary: '사용자 생성',
+  })
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST,
+    description: '이미 존재하는 account/name 입니다.',
+  })
+  async postUser(@Body() body: CreateUserDto): Promise<CreateUserDto> {
+    return this.usersService.createUser(body);
+  }
 }
