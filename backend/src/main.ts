8a8c0dc45f2330e0c2c7c96e8993537000c96795
eeseung <eeseungh@gmail.com>
2024-06-10 19:09:45 +0900

    feat: Swagger BasicAuth 설정

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import * as expressBasicAuth from 'express-basic-auth';
+import expressBasicAuth from 'express-basic-auth';
 import {
   ENV_SWAGGER_PASSWORD_KEY,
   ENV_SWAGGER_USER_KEY,
@@ -37,9 +37,14 @@ async function bootstrap() {
     .setTitle('EMR API')
     .setDescription('FREEMED EMR (Electronic Medical Record) API')
     .setVersion('2.0')
+    .addBasicAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/docs', app, document);
+  SwaggerModule.setup('api/docs', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
 
   await app.listen(parseInt(process.env.PORT, 10) || 3000);
 }
