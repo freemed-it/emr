36c78cb146ddc948bbab3bebbe15fa2d0702f745
kjjlss2 <jgim0417@gmail.com>
2024-06-10 22:58:46 +0900

    feat: 참여자 접수 API 구현

---
+++ backend/src/patients/dto/create-patient.dto.ts
@@ -0,0 +1,100 @@
+import {
+  IsDate,
+  IsEnum,
+  IsInt,
+  IsNumber,
+  IsString,
+  Length,
+  Min,
+} from 'class-validator';
+import { Gender } from '../const/gender.const';
+import { ApiProperty, PickType } from '@nestjs/swagger';
+import { Patients } from '../entity/patients.entity';
+import { stringValidationMessage } from 'src/common/validation-message/string-validation.message';
+import { lengthValidationMessage } from 'src/common/validation-message/length-validation.message';
+
+export class CreatePatientDto extends PickType(Patients, [
+  'firstVisit',
+  'name',
+  'gender',
+  'birth',
+  'height',
+  'weight',
+  'bmi',
+  'smokingAmount',
+  'smokingPeriod',
+  'drinkingAmount',
+  'drinkingPeriod',
+]) {
+  @IsDate()
+  firstVisit: Date;
+
+  @ApiProperty({
+    description: '이름',
+    example: '홍길동',
+  })
+  @IsString({ message: stringValidationMessage })
+  @Length(2, 10, { message: lengthValidationMessage })
+  name: string;
+
+  @IsEnum(Gender)
+  gender: Gender;
+
+  @ApiProperty({
+    description: '생년월일',
+    example: '2000-04-05',
+  })
+  @IsString({ message: stringValidationMessage })
+  birth: string;
+
+  @ApiProperty({
+    description: '신장',
+    example: '180',
+  })
+  @Min(1)
+  @IsInt()
+  height: number;
+
+  @ApiProperty({
+    description: '체중',
+    example: '80',
+  })
+  @Min(1)
+  @IsInt()
+  weight: number;
+
+  @IsNumber()
+  bmi: number;
+
+  @ApiProperty({
+    description: '흡연량',
+    example: '1',
+  })
+  @Min(1)
+  @IsInt()
+  smokingAmount: number;
+
+  @ApiProperty({
+    description: '흡연 경력',
+    example: '2',
+  })
+  @Min(1)
+  @IsInt()
+  smokingPeriod: number;
+
+  @ApiProperty({
+    description: '음주량',
+    example: '3',
+  })
+  @Min(1)
+  @IsInt()
+  drinkingAmount: number;
+
+  @ApiProperty({
+    description: '음주 경력',
+    example: '4',
+  })
+  @Min(1)
+  @IsInt()
+  drinkingPeriod: number;
+}
--- backend/src/patients/patients.controller.ts
+++ backend/src/patients/patients.controller.ts
@@ -1,7 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post } from '@nestjs/common';
 import { PatientsService } from './patients.service';
+import { CreatePatientDto } from './dto/create-patient.dto';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { Department } from 'src/orders/const/department.const';
 
 @Controller('patients')
 export class PatientsController {
   constructor(private readonly patientsService: PatientsService) {}
+
+  @Post('/m/receipt')
+  @ApiOperation({
+    summary: '의과 참여자 접수 생성',
+  })
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST,
+    description: '이름/생년월일을 입력해주세요.',
+  })
+  async createMedicalPatient(@Body() createPatientDto: CreatePatientDto) {
+    return this.patientsService.createPatient(createPatientDto, Department.M);
+  }
+
+  @Post('/km/receipt')
+  @ApiOperation({
+    summary: '한의과 참여자 접수 생성',
+  })
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST,
+    description: '이름/생년월일을 입력해주세요.',
+  })
+  async createKoreanMedicalPatient(@Body() createPatientDto: CreatePatientDto) {
+    return this.patientsService.createPatient(createPatientDto, Department.KM);
+  }
 }
