f8946218e3aeeab4b2fe1930c671605b5f83b39a
eeseung <eeseungh@gmail.com>
2024-06-12 00:00:10 +0900

    feat(BE): GlobalPrefix 설정

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -12,6 +12,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix('api/v2');
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
