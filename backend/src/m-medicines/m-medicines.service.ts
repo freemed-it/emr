23822b02b9f728036112f371efeeb6ba3a3fb8a1
eeseung <eeseungh@gmail.com>
2024-06-17 20:12:20 +0900

    feat: categoryId에 해당하는 약품 삭제 확인하는 메서드 구현

--- backend/src/m-medicines/m-medicines.service.ts
+++ backend/src/m-medicines/m-medicines.service.ts
@@ -1,4 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { IsNull, Not, Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { M_Medicines } from './entity/m-medicines.entity';
 
 @Injectable()
-export class MMedicinesService {}
+export class MMedicinesService {
+  constructor(
+    @InjectRepository(M_Medicines)
+    private readonly mMedicinesRepository: Repository<M_Medicines>,
+  ) {}
+
+  async checkDeletedMMedicinesByCategoryId(categoryId: number) {
+    const [, deletedMedicineCount] =
+      await this.mMedicinesRepository.findAndCount({
+        where: {
+          category: {
+            id: categoryId,
+          },
+          deletedAt: Not(IsNull()),
+        },
+        withDeleted: true,
+      });
+
+    const [, medicineCount] = await this.mMedicinesRepository.findAndCount({
+      where: {
+        category: {
+          id: categoryId,
+        },
+      },
+      withDeleted: true,
+    });
+
+    return deletedMedicineCount === medicineCount;
+  }
+}
