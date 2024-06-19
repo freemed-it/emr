678242e0682f06686938ad4637a5826a5d6e6c45
eeseung <eeseungh@gmail.com>
2024-06-19 15:20:50 +0900

    feat: 의과 약품 생성 API 구현

--- backend/src/m-medicines/m-medicines.controller.ts
+++ backend/src/m-medicines/m-medicines.controller.ts
@@ -1,7 +1,44 @@
-import { Controller } from '@nestjs/common';
+import {
+  Post,
+  UseInterceptors,
+  Controller,
+  UploadedFile,
+  Body,
+  HttpStatus,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import {
+  ApiOperation,
+  ApiConsumes,
+  ApiTags,
+  ApiResponse,
+} from '@nestjs/swagger';
 import { MMedicinesService } from './m-medicines.service';
+import { CreateMMedicineDto } from './dto/create-m-medicine.dto';
 
+@ApiTags('의과')
 @Controller('m/medicines')
 export class MMedicinesController {
   constructor(private readonly mMedicinesService: MMedicinesService) {}
+
+  @Post()
+  @UseInterceptors(FileInterceptor('image'))
+  @ApiConsumes('multipart/form-data')
+  @ApiOperation({
+    summary: '약품 생성',
+  })
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST,
+    description:
+      '존재하지 않는 분류입니다. <small>categoryId에 해당하는 분류가 없는 경우</small>',
+  })
+  async postMMedicine(
+    @Body() createMMedicineDto: CreateMMedicineDto,
+    @UploadedFile() image: Express.Multer.File,
+  ) {
+    return await this.mMedicinesService.createMMedicine(
+      createMMedicineDto,
+      image,
+    );
+  }
 }
