78bef7b822c70e72efd411e4b07719632ad5c938
kjjlss2 <jgim0417@gmail.com>
2024-06-25 22:25:38 +0900

    feat: 의과 예진, 바이탈사인 생성 DTO 추가

--- backend/src/m-charts/entity/m-charts.entity.ts
+++ backend/src/m-charts/entity/m-charts.entity.ts
@@ -1,4 +1,3 @@
-import { IsInt, IsNumber, IsString } from 'class-validator';
 import { BaseModel } from 'src/common/entity/base.entity';
 import { M_Complaints } from 'src/m-complaints/entity/m-complaints.entity';
 import { M_Prescriptions } from 'src/m-prescriptions/entity/m-prescriotions.entity';
@@ -18,83 +17,70 @@ export class M_Charts extends BaseModel {
     type: 'bigint',
     unique: true,
   })
-  @IsString()
   chartNumber: string;
 
   @Column({
     default: 1,
   })
-  @IsInt()
   status: number;
 
   @Column({
     nullable: true,
   })
-  @IsNumber()
   spO2: number;
 
   @Column({
     nullable: true,
   })
-  @IsInt()
   heartRate: number;
 
   @Column('float', {
     nullable: true,
   })
-  @IsNumber()
   bodyTemperature: number;
 
   @Column({
     nullable: true,
   })
-  @IsInt()
   systoleBloodPressure: number;
 
   @Column({
     nullable: true,
   })
-  @IsInt()
   diastoleBloodPressure: number;
 
   @Column({
     nullable: true,
   })
-  @IsInt()
   bloodGlucose: number;
 
   @Column({
     nullable: true,
   })
-  @IsInt()
   afterMeals: number;
 
   @Column({
     length: 500,
     nullable: true,
   })
-  @IsString()
   impression: string;
 
   @Column({
     length: 500,
     nullable: true,
   })
-  @IsString()
   presentIllness: string;
 
   @Column({
     length: 500,
     nullable: true,
   })
-  @IsString()
   treatmentNote: string;
 
   @Column({
     length: 30,
     nullable: true,
   })
-  @IsString()
   vsMemo: string;
 
   @OneToMany(() => M_Complaints, (complaint) => complaint.chart)
