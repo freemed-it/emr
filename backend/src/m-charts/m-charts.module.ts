f5d0887b613325c38d9b1196d15ca5f571b92bbd
kjjlss2 <jgim0417@gmail.com>
2024-06-25 22:31:18 +0900

    feat: 의과 예진 생성 API 구현

--- backend/src/m-charts/m-charts.module.ts
+++ backend/src/m-charts/m-charts.module.ts
@@ -3,10 +3,16 @@ import { MChartsService } from './m-charts.service';
 import { MChartsController } from './m-charts.controller';
 import { M_Charts } from './entity/m-charts.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { M_Complaints } from 'src/m-complaints/entity/m-complaints.entity';
+import { MComplaintsService } from 'src/m-complaints/m-complaints.service';
+import { Histories } from 'src/patients/histories/entity/histories.entity';
+import { Orders } from 'src/orders/entity/orders.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([M_Charts])],
+  imports: [
+    TypeOrmModule.forFeature([M_Charts, Histories, M_Complaints, Orders]),
+  ],
   controllers: [MChartsController],
-  providers: [MChartsService],
+  providers: [MChartsService, MComplaintsService],
 })
 export class MChartsModule {}
--- backend/src/m-charts/m-charts.service.ts
+++ backend/src/m-charts/m-charts.service.ts
@@ -1,4 +1,98 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { M_Charts } from './entity/m-charts.entity';
+import { CreateVitalSignDto } from './dto/create-vital-sign.dto';
+import { M_Complaints } from 'src/m-complaints/entity/m-complaints.entity';
+import { CreateMComplaintDto } from '../m-complaints/dto/create-m-complaint.dto';
+import { CreateHistoryDto } from 'src/patients/histories/dto/create-history.dto';
+import { Histories } from 'src/patients/histories/entity/histories.entity';
+import { Orders } from 'src/orders/entity/orders.entity';
 
 @Injectable()
-export class MChartsService {}
+export class MChartsService {
+  constructor(
+    @InjectRepository(M_Charts)
+    private readonly chartsRepository: Repository<M_Charts>,
+    @InjectRepository(Histories)
+    private readonly historiesRepository: Repository<Histories>,
+    @InjectRepository(M_Complaints)
+    private readonly complaintsRepository: Repository<M_Complaints>,
+    @InjectRepository(Orders)
+    private readonly ordersRepository: Repository<Orders>,
+  ) {}
+
+  async createVitalSign(chartId: number, vitalSignDto: CreateVitalSignDto) {
+    const vistalSign = await this.chartsRepository.findOne({
+      where: { id: chartId },
+    });
+
+    if (!vistalSign) {
+      throw new NotFoundException();
+    }
+
+    Object.assign(vistalSign, vitalSignDto);
+    return this.chartsRepository.save(vistalSign);
+  }
+
+  async createComplaint(chartId: number, complaintDto: CreateMComplaintDto) {
+    const complaint = this.complaintsRepository.create({
+      ...complaintDto,
+      chart: { id: chartId } as any,
+    });
+
+    return this.complaintsRepository.save(complaint);
+  }
+
+  async createHistory(chartId: number, historyDto: CreateHistoryDto) {
+    const chart = await this.chartsRepository.findOne({
+      where: { id: chartId },
+      relations: { patient: true },
+    });
+
+    if (!chart) {
+      throw new NotFoundException();
+    }
+
+    const existingHistory = await this.historiesRepository.findOne({
+      where: { patient: { id: chart.patient.id } },
+    });
+
+    if (existingHistory) {
+      Object.assign(existingHistory, historyDto);
+      return this.historiesRepository.save(existingHistory);
+    } else {
+      const newHistory = this.historiesRepository.create({
+        ...historyDto,
+        patient: { id: chart.patient.id },
+      });
+      return this.historiesRepository.save(newHistory);
+    }
+  }
+
+  async updateStatus(chartId: number) {
+    const chart = await this.chartsRepository.findOne({
+      where: { id: chartId },
+    });
+
+    if (!chart) {
+      throw new NotFoundException();
+    }
+
+    chart.status = 2;
+    await this.chartsRepository.save(chart);
+
+    const orders = await this.ordersRepository.find({
+      where: { mChart: { id: chartId } },
+    });
+
+    if (orders.length > 0) {
+      orders.forEach((order) => {
+        order.status = 2;
+      });
+      await this.ordersRepository.save(orders);
+    }
+
+    return chart;
+  }
+}
