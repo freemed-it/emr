37e615debdaea5276355d04128910650e45a920f
eeseung <eeseungh@gmail.com>
2024-06-30 13:26:19 +0900

    feat: 의과 처방 생성 API 예외 처리

--- backend/src/m-prescriptions/m-prescriptions.module.ts
+++ backend/src/m-prescriptions/m-prescriptions.module.ts
@@ -3,9 +3,11 @@ import { MPrescriptionsService } from './m-prescriptions.service';
 import { MPrescriptionsController } from './m-prescriptions.controller';
 import { M_Prescriptions } from './entity/m-prescriotions.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { M_Charts } from 'src/m-charts/entity/m-charts.entity';
+import { M_Medicines } from 'src/m-medicines/entity/m-medicines.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([M_Prescriptions])],
+  imports: [TypeOrmModule.forFeature([M_Prescriptions, M_Charts, M_Medicines])],
   controllers: [MPrescriptionsController],
   providers: [MPrescriptionsService],
   exports: [MPrescriptionsService],
--- backend/src/m-prescriptions/m-prescriptions.service.ts
+++ backend/src/m-prescriptions/m-prescriptions.service.ts
@@ -1,21 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { M_Prescriptions } from './entity/m-prescriotions.entity';
 import { Repository } from 'typeorm';
 import { CreateMPrescriptionDto } from './dto/create-m-prescription.dto';
+import { M_Charts } from 'src/m-charts/entity/m-charts.entity';
+import { M_Medicines } from 'src/m-medicines/entity/m-medicines.entity';
 
 @Injectable()
 export class MPrescriptionsService {
   constructor(
+    @InjectRepository(M_Charts)
+    private readonly mChartsRepository: Repository<M_Charts>,
     @InjectRepository(M_Prescriptions)
     private readonly mPrescriptionsRepository: Repository<M_Prescriptions>,
+    @InjectRepository(M_Medicines)
+    private readonly mMedicinesRepository: Repository<M_Medicines>,
   ) {}
 
   async createMPrescription(
     chartId: number,
     prescriptionDto: CreateMPrescriptionDto,
   ) {
+    const chart = await this.mChartsRepository.findOne({
+      where: {
+        id: chartId,
+      },
+    });
+
+    if (!chart) {
+      throw new NotFoundException();
+    }
+
     const { medicineId, ...restPrescriptionDto } = prescriptionDto;
+    const medicine = await this.mMedicinesRepository.findOne({
+      where: {
+        id: medicineId,
+      },
+    });
+
+    if (!medicine) {
+      throw new BadRequestException('존재하지 않는 약품입니다.');
+    }
 
     return await this.mPrescriptionsRepository.save({
       ...restPrescriptionDto,
