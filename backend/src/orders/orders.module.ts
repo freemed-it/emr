63e831edadcdbbe0d7846abf0038be2f85f8846e
kjjlss2 <jgim0417@gmail.com>
2024-07-01 23:53:40 +0900

    feat: OCS 목록 조회 API 구현

--- backend/src/orders/orders.module.ts
+++ backend/src/orders/orders.module.ts
@@ -3,9 +3,10 @@ import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
 import { Orders } from './entity/orders.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CommonModule } from 'src/common/common.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Orders])],
+  imports: [TypeOrmModule.forFeature([Orders]), CommonModule],
   controllers: [OrdersController],
   providers: [OrdersService],
 })
--- backend/src/orders/orders.service.ts
+++ backend/src/orders/orders.service.ts
@@ -1,4 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Orders } from './entity/orders.entity';
+import { Between, Repository } from 'typeorm';
+import { CommonService } from 'src/common/common.service';
+import { PaginateOrderDto } from './dto/paginate-order.dto';
 
 @Injectable()
-export class OrdersService {}
+export class OrdersService {
+  constructor(
+    @InjectRepository(Orders)
+    private readonly ordersRepository: Repository<Orders>,
+    private readonly commonService: CommonService,
+  ) {}
+
+  async paginateOrders(
+    startDate: string,
+    endDate: string,
+    paginateDto: PaginateOrderDto,
+  ) {
+    const start = new Date(startDate);
+    const end = new Date(endDate);
+
+    if (start.getTime() === end.getTime()) {
+      end.setDate(end.getDate() + 1);
+    }
+    if (start.getTime() > end.getTime()) {
+      throw new BadRequestException('날짜를 올바르게 설정해주세요.');
+    }
+    return this.commonService.paginate(paginateDto, this.ordersRepository, {
+      where: {
+        createdAt: Between(start, end),
+        ...(paginateDto.department && { department: paginateDto.department }),
+        ...(paginateDto.status !== undefined && { status: paginateDto.status }),
+        ...(paginateDto.gender && { patient: { gender: paginateDto.gender } }),
+      },
+      relations: {
+        patient: true,
+      },
+    });
+  }
+}
