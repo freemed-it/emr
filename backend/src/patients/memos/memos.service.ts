d81c40e85592747bc2776544ede7a10e3825b807
Jaei Kim <98146849+yeeezae@users.noreply.github.com>
2024-07-11 11:34:10 +0900

    feat(BE): 참여자 메모 수정 API 구현 (#74)
    feat: 참여자 메모 수정 API 구현

--- backend/src/patients/memos/memos.service.ts
+++ backend/src/patients/memos/memos.service.ts
@@ -1,31 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Memos } from './entity/memos.entity';
-import { Patients } from '../entity/patients.entity';
-import { Repository } from 'typeorm';
-import { CreateMemoDto } from './dto/create-memo.dto';
-import { Users } from 'src/users/entity/users.entity';
 
 @Injectable()
-export class MemosService {
-  constructor(
-    @InjectRepository(Memos)
-    private memosRepository: Repository<Memos>,
-    @InjectRepository(Patients)
-    private patientsRepository: Repository<Patients>,
-  ) {}
-
-  async createMemo(patientId: number, memoDto: CreateMemoDto, user: Users) {
-    const patient = await this.patientsRepository.findOne({
-      where: { id: patientId },
-    });
-
-    const memo = this.memosRepository.create({
-      ...memoDto,
-      patient,
-      account: user.account,
-    });
-
-    return this.memosRepository.save(memo);
-  }
-}
+export class MemosService {}
--- backend/src/patients/patients.module.ts
+++ backend/src/patients/patients.module.ts
@@ -6,10 +6,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { M_Charts } from 'src/m-charts/entity/m-charts.entity';
 import { KM_Charts } from 'src/km-charts/entity/km-charts.entity';
 import { Orders } from 'src/orders/entity/orders.entity';
+import { Memos } from './memos/entity/memos.entity';
+import { MemosService } from './memos/memos.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Patients, M_Charts, KM_Charts, Orders])],
+  imports: [
+    TypeOrmModule.forFeature([Patients, M_Charts, KM_Charts, Orders, Memos]),
+  ],
   controllers: [PatientsController],
-  providers: [PatientsService],
+  providers: [PatientsService, MemosService],
 })
 export class PatientsModule {}
