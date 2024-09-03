41fed37bf43782b42e0fd5aa03dc38c46afe6b9e
eeseung <eeseungh@gmail.com>
2024-09-03 22:06:02 +0900

    feat: 한의과 약품 생성 API 구현

--- backend/src/km-medicines/km-medicines.service.ts
+++ backend/src/km-medicines/km-medicines.service.ts
@@ -1,4 +1,65 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { KM_Medicines } from './entity/km-medicines.entity';
+import { Repository } from 'typeorm';
+import { CreateKMMedicineDto } from './dto/create-km-medicine.dto';
+import { ConfigService } from '@nestjs/config';
+import {
+  ObjectCannedACL,
+  PutObjectCommand,
+  S3Client,
+} from '@aws-sdk/client-s3';
 
 @Injectable()
-export class KmMedicinesService {}
+export class KmMedicinesService {
+  constructor(
+    @InjectRepository(KM_Medicines)
+    private readonly kmMedicinesRepository: Repository<KM_Medicines>,
+    private readonly configService: ConfigService,
+  ) {}
+
+  async createMedicine(
+    medicineDto: CreateKMMedicineDto,
+    image: Express.Multer.File,
+  ) {
+    const imagePath = await this.uploadImage(image, medicineDto.name);
+
+    return await this.kmMedicinesRepository.save({
+      ...medicineDto,
+      image: imagePath,
+    });
+  }
+
+  async uploadImage(image: Express.Multer.File, name: string) {
+    if (!image) return;
+
+    const s3Region = this.configService.get('AWS_S3_REGION');
+    const s3ImageBucket = this.configService.get('AWS_S3_IMAGE_BUCKET_NAME');
+    const client = new S3Client({
+      region: s3Region,
+      credentials: {
+        accessKeyId: this.configService.get('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: this.configService.get('AWS_SECRET_ACCESS_KEY'),
+      },
+    });
+    const env = process.env.NODE_ENV === 'production' ? 'prod' : 'dev';
+    const ext = image.mimetype.split('/')[1];
+    const key = `${env}/km-medicines/${name}-${Date.now()}.${ext}`;
+
+    try {
+      await client.send(
+        new PutObjectCommand({
+          Key: key,
+          Body: image.buffer,
+          Bucket: s3ImageBucket,
+          ACL: ObjectCannedACL.public_read,
+          ContentType: image.mimetype,
+        }),
+      );
+
+      return `https://${s3ImageBucket}.s3.${s3Region}.amazonaws.com/${key}`;
+    } catch (error) {
+      throw new BadRequestException(error);
+    }
+  }
+}
