26651a5b256d0674553798ac67652cc735dad938
kjjlss2 <jgim0417@gmail.com>
2024-09-03 21:31:30 +0900

    feat: 한의과 예진 생성 API 구현

--- backend/src/km-complaints/entity/km-complaints.entity.ts
+++ backend/src/km-complaints/entity/km-complaints.entity.ts
@@ -1,4 +1,4 @@
-import { IsString, Length } from 'class-validator';
+import { Length } from 'class-validator';
 import { BaseModel } from 'src/common/entity/base.entity';
 import { KM_Charts } from 'src/km-charts/entity/km-charts.entity';
 import { Patients } from 'src/patients/entity/patients.entity';
@@ -24,20 +24,17 @@ export class KM_Complaints extends BaseModel {
     length: 20,
     nullable: true,
   })
-  @IsString()
   category: string;
 
   @Column({
     length: 100,
   })
   @Length(0, 100)
-  @IsString()
   cheifComplaint: string;
 
   @Column({
     length: 500,
     nullable: true,
   })
-  @IsString()
   cheifComplaintHistory: string;
 }
