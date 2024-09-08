389be3732f17b90ee4b902e608ba519893bbf5df
eeseung <eeseungh@gmail.com>
2024-09-08 20:19:29 +0900

    docs: 한의과 처방 수정 API Response 수정

--- backend/src/km-prescriptions/km-prescriptions.controller.ts
+++ backend/src/km-prescriptions/km-prescriptions.controller.ts
@@ -11,31 +11,22 @@ import {
 import { KmPrescriptionsService } from './km-prescriptions.service';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { UpdateKMPrescriptionDto } from './dto/update-km-prescription.dto';
-import { KmMedicinesService } from 'src/km-medicines/km-medicines.service';
 
 @ApiTags('한의과')
 @Controller('km/prescriptions')
 export class KmPrescriptionsController {
-  constructor(
-    private readonly prescriptionsService: KmPrescriptionsService,
-    private readonly medicinesService: KmMedicinesService,
-  ) {}
+  constructor(private readonly prescriptionsService: KmPrescriptionsService) {}
 
   @Patch(':prescriptionId')
   @ApiOperation({
     summary: '처방 수정',
   })
   @ApiResponse({
-    status: HttpStatus.BAD_REQUEST,
-    description:
-      '존재하지 않는 약품입니다. <small>medicineId에 해당하는 약품이 없는 경우</small>',
-  })
-  @ApiResponse({
     status: HttpStatus.NOT_FOUND,
   })
   async patchPrescription(
     @Param('prescriptionId', ParseIntPipe) prescriptionId: number,
-    @Body() updatePrescriptioneDto: UpdateKMPrescriptionDto,
+    @Body() updatePrescriptionDto: UpdateKMPrescriptionDto,
   ) {
     const prescriptionExists =
       await this.prescriptionsService.checkPrescriptionExistsById(
@@ -47,7 +38,7 @@ export class KmPrescriptionsController {
 
     return this.prescriptionsService.updatePrescription(
       prescriptionId,
-      updatePrescriptioneDto,
+      updatePrescriptionDto,
     );
   }
 
