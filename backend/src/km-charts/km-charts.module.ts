b543b7435e8e5ba1929679e8e57618724c0b59fb
Jaei Kim <98146849+yeeezae@users.noreply.github.com>
2024-09-08 19:34:47 +0900

    feat(BE): 한의과 예진 조회 API 수정 (#103)
    feat: 한의과 예진 조회 API 수정

--- backend/src/km-charts/km-charts.module.ts
+++ backend/src/km-charts/km-charts.module.ts
@@ -6,17 +6,20 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Histories } from 'src/patients/histories/entity/histories.entity';
 import { KM_Complaints } from 'src/km-complaints/entity/km-complaints.entity';
 import { Orders } from 'src/orders/entity/orders.entity';
-import { MComplaintsService } from 'src/m-complaints/m-complaints.service';
 import { KmPrescriptionsService } from 'src/km-prescriptions/km-prescriptions.service';
 import { KmMedicinesService } from 'src/km-medicines/km-medicines.service';
 import { KM_Prescriptions } from 'src/km-prescriptions/entity/km-prescriptions.entity';
 import { KM_Medicines } from 'src/km-medicines/entity/km-medicines.entity';
 import { CommonService } from 'src/common/common.service';
+import { M_Charts } from 'src/m-charts/entity/m-charts.entity';
+import { KmComplaintsService } from 'src/km-complaints/km-complaints.service';
+import { OrdersService } from 'src/orders/orders.service';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([
       KM_Charts,
+      M_Charts,
       Histories,
       KM_Complaints,
       Orders,
@@ -27,9 +30,10 @@ import { CommonService } from 'src/common/common.service';
   controllers: [KmChartsController],
   providers: [
     KmChartsService,
-    MComplaintsService,
+    KmComplaintsService,
     KmPrescriptionsService,
     KmMedicinesService,
+    OrdersService,
     CommonService,
   ],
 })
