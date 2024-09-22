1f595467fa284b1ce03bf80294880de108538761
kjjlss2 <jgim0417@gmail.com>
2024-09-22 09:35:52 +0900

    refactor: 참여자 히스토리 생성 service 구분

--- backend/src/patients/histories/histories.module.ts
+++ backend/src/patients/histories/histories.module.ts
@@ -3,9 +3,11 @@ import { HistoriesService } from './histories.service';
 import { HistoriesController } from './histories.controller';
 import { Histories } from '../entity/histories.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { MCharts } from 'src/m/entity/charts.entity';
+import { KmCharts } from 'src/km/entity/charts.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Histories])],
+  imports: [TypeOrmModule.forFeature([Histories, MCharts, KmCharts])],
   controllers: [HistoriesController],
   providers: [HistoriesService],
 })
--- backend/src/patients/histories/histories.service.ts
+++ backend/src/patients/histories/histories.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Histories } from '../entity/histories.entity';
+import { CreateHistoryDto } from './dto/create-history.dto';
 
 @Injectable()
-export class HistoriesService {}
+export class HistoriesService {
+  constructor(
+    @InjectRepository(Histories)
+    private readonly historiesRepository: Repository<Histories>,
+  ) {}
+
+  async createHistory(patientId: number, historyDto: CreateHistoryDto) {
+    const history = await this.historiesRepository.findOne({
+      where: { patient: { id: patientId } },
+    });
+
+    return await this.historiesRepository.save(
+      history ? { ...history, ...historyDto } : historyDto,
+    );
+  }
+}
